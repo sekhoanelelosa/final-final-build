963842e6aedd2d5e08b5504bbdf2989114591e46
sekhoane <sekhoane@DESKTOP-7AOHI0O>
2022-10-17 19:44:57 +0200

    product

---
+++ Store_Online/Store_Online.DataAccess.Inmemory/ProductRepository.cs
@@ -0,0 +1,78 @@
+﻿using Store_Online.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_Online.DataAccess.Inmemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.ID == product.ID);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not Found");
+            }
+        }
+        public Product Find(string ID)
+        {
+            Product product = products.Find(p => p.ID == ID);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not Found");
+
+            }
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        public void Delete(string ID)
+        {
+            Product productToDelete = products.Find(p => p.ID == ID);
+            if(productToDelete !=null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+}
+    
+
